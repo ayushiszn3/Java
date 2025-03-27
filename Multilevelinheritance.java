a6753c0d3dbd1d7b560db719a44622c68b661c81
ayushi sharma <143016763+ayushiszn3@users.noreply.github.com>
2025-03-27 22:29:00 +0530

    Update Multilevelinheritance.java

--- Multilevelinheritance.java
+++ Multilevelinheritance.java
@@ -1,19 +1,28 @@
-class One{
-    public void print_geek(){
+class One {
+    public void print_geek() {
         System.out.println("Geeks");
     }
 }
+
 class Two extends One {
-    public void print_for(){
+    public void print_for() {
         System.out.println("for");
     }
 }
+
 class Three extends Two {
-    public void print_lastgeek(){
-        System.out.println();
+    public void print_lastgeek() {
+        System.out.println("Geeks");
     }
 }
 
 public class Multilevelinheritance {
-    
+    public static void main(String[] args) {
+        Three obj = new Three(); // Creating object of class Three
+        
+        // Calling methods from all three classes
+        obj.print_geek();       // From class One
+        obj.print_for();        // From class Two
+        obj.print_lastgeek();   // From class Three
+    }
 }
